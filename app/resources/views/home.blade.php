dc041be7821d1b2ff2edee7ef19c4667183be567
Humayun Rahman <mahy.r9@gmail.com>
2021-11-04 18:06:16 -0400

    css

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World!</title>
+    <link rel="stylesheet" href="C:\Users\mahy\Documents\HRTechnologies\app\resources\css" />
+  </head>
+  <body>
+      <h1 class="title">Hello World! </h1>
+      <p id="currentTime"></p>
+      <script src="script.js"></script>
+  </body>
+</html>
--- app/routes/web.php
+++ app/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
