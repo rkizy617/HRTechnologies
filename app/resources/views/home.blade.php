d250c444b25a64165ca54fdb5dd38f1d53622308
slewandowski99 <91228651+slewandowski99@users.noreply.github.com>
2021-11-08 17:58:46 -0500

    Testing New Homepage

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -1,13 +1,62 @@
-<!--Home page-->
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Hello World!</title>
-    <link rel="stylesheet" href="C:\Users\mahy\Documents\HRTechnologies\app\resources\css" />
-  </head>
-  <body>
-      <h1 class="title">Hello World! </h1>
-      <p id="currentTime"></p>
-      <script src="script.js"></script>
-  </body>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="index2.css">
+    <title>Home - HRTechnologies</title>
+</head>
+<header>
+    <div class="topnav">
+    <h1>HRTechnologies</h1>
+    <button type="button" class="cancelbtn"><a href="main.html">Logout</a></button>
+
+    </div>
+
+
+</header>
+<body>
+    <div class="sidenav">
+        <img src="HRLOGO.png" alt="logo">
+        <br><br>
+        <a href="index2.html">Home</a>
+        <a href="assignments.html">Assignments</a>
+        <a href="schedule.html">Schedule</a>
+        <a href="contact.html">Contact</a>
+        <a href="settings.html">Settings</a>
+        <a href="employees.html">Employees</a>
+    </div>
+
+
+
+    <div class="main">
+        <h2>Welcome to HRTechnolgies!</h2>
+        <h3>Company Updates</h3>
+            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
+                concludaturque et eum,
+                altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+                repudiandae nec et.
+                Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <h3>Company Updates</h3>
+            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
+                concludaturque et eum,
+                altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+                repudiandae nec et.
+                Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <h3>Company Updates</h3>
+            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
+                concludaturque et eum,
+                altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+                repudiandae nec et.
+                Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <h3>Company Updates</h3>
+            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
+                concludaturque et eum,
+                altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
+                repudiandae nec et.
+                Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+    </div>
+
+</body>
+
 </html>
