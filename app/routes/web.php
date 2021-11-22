943ab0ea591debd9e64cf729abaf790cabb5b2dd
Humayun Rahman <mahy.r9@gmail.com>
2021-11-22 13:27:09 -0500

    2nd sprint update

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,4 +22,7 @@ Route::get('/welcome', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::get('/AssignEmployees', function () {
+    return view('AssignEmployees');
+});
 //changed the main route  to home page and / added /welcome  route for welcome page
